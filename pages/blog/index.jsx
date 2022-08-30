9d2e0ebb769ccebae256220f33891db1201c7eef
taran <staranbeer@gmail.com>
2022-08-31 03:44:41 +0530

    added ssg to blog

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { env } from "process";
 import BlogList from "../../components/Blog/BlogList";
 import Header from "../../components/Header/Header";
+import fs from "fs";
+import path from "path";
+
 import Hero from "../../components/Hero/Hero";
 import Filters from "../../components/Filters/Filters";
 import Featured from "../../components/Featured/Featured";
@@ -29,18 +32,20 @@ const Index = ({ blogs }) => {
 };
 
 export async function getStaticProps() {
-  let res;
   try {
-    res = await fetch(`${env.URL}/api/blog`);
-    res = await res.json();
+    let res;
+    res = fs.readFileSync(
+      path.join(process.cwd(), "data", "blogs.json"),
+      "utf8",
+    );
+    return {
+      props: {
+        blogs: JSON.parse(res.data),
+      },
+    };
   } catch (err) {
     console.log(err.message);
   }
-  return {
-    props: {
-      blogs: JSON.parse(res.data),
-    },
-  };
 }
 
 export default Index;
