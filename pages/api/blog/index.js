2c506afabe005096eddb7752deab46c23e337abc
taran <staranbeer@gmail.com>
2022-08-31 01:40:32 +0530

    integrated the pexels api

--- pages/api/blog/index.js
+++ pages/api/blog/index.js
@@ -5,5 +5,5 @@ import { cwd } from "process";
 export default function handler(req, res) {
   const filePath = path.join(cwd(), "data", "blogs.json");
   const blogs = fs.readFileSync(filePath, "utf8");
-  res.status(200).json({ data: JSON.parse(blogs) });
+  res.status(200).json({ data: blogs });
 }
--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,47 +1,44 @@
+import Image from "next/image";
 import React from "react";
 
 const Slug = (props) => {
-  const { title } = props.data;
+  const { title, content, image } = props.data;
+  console.log(image);
   if (!props.data) {
     return <div>Nothing found</div>;
   }
   return (
-    <div className="bg-red-400 ">
+    <div className=" ">
       {/* thumbnail */}
-      <div></div>
+      <div className="relative bg-blue-400">
+        <div className="w-full relative h-[400px]">
+          <img
+            src={image.src.landscape}
+            alt={image.alt}
+            className="w-full h-full object-cover"
+          />
+        </div>
+        <div className="absolute bottom-0 px-1 m-1 right-0 bg-white">
+          Image by {image.photographer} on{" "}
+          <a href="https://pexels.com" className="underline">
+            pexels
+          </a>
+        </div>
+      </div>
       {/* title */}
-      <h1 className="text-3xl font-medium">{title}</h1>
+      <div className="flex justify-between py-4 text-gray-500 ">
+        <div>Author: </div>
+        <div>TimeStamp:</div>
+      </div>
+      <h1 className="text-3xl font-medium mt-6">{title}</h1>
       {/* desciption */}
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, natus?
-        Eaque distinctio dolorum doloremque est ipsa ducimus voluptatum sint
-        corrupti modi, soluta omnis enim quas reprehenderit, recusandae quo
-        debitis tempore.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, natus?
-        Eaque distinctio dolorum doloremque est ipsa ducimus voluptatum sint
-        corrupti modi, soluta omnis enim quas reprehenderit, recusandae quo
-        debitis tempore.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, natus?
-        Eaque distinctio dolorum doloremque est ipsa ducimus voluptatum sint
-        corrupti modi, soluta omnis enim quas reprehenderit, recusandae quo
-        debitis tempore.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, natus?
-        Eaque distinctio dolorum doloremque est ipsa ducimus voluptatum sint
-        corrupti modi, soluta omnis enim quas reprehenderit, recusandae quo
-        debitis tempore.
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, natus?
-        Eaque distinctio dolorum doloremque est ipsa ducimus voluptatum sint
-        corrupti modi, soluta omnis enim quas reprehenderit, recusandae quo
-        debitis tempore.
-      </p>
+      <div className="mt-3">
+        {content} Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Numquam nesciunt provident aliquam? Ex, eius tenetur quae labore
+        voluptatum harum accusamus cupiditate provident nobis illum quasi,
+        consectetur quidem, suscipit quos. Itaque quo iure aut aliquid, ex nihil
+        libero debitis at saepe!
+      </div>
     </div>
   );
 };
@@ -51,9 +48,20 @@ export async function getStaticProps({ params }) {
   let res = await fetch(`${process.env.URL}/api/blog/${slug}`);
   res = await res.json();
   res = res.data;
+  let image = await fetch(
+    "https://api.pexels.com/v1/search?query=nature&page=1&per_page=1",
+    {
+      headers: {
+        Authorization: `${process.env.PEXELS_API_KEY}`,
+      },
+    },
+  );
+
+  image = await image.json();
+  image = await image.photos;
   return {
     props: {
-      data: res,
+      data: { ...res, image: image[0] },
     },
   };
 }
@@ -61,8 +69,8 @@ export async function getStaticProps({ params }) {
 export async function getStaticPaths() {
   const res = await fetch(`${process.env.URL}/api/blog`);
   let blogs = await res.json();
-  blogs = blogs.data;
-  const paths = blogs.map((blog) => ({
+  blogs = await blogs.data;
+  const paths = JSON.parse(blogs).map((blog) => ({
     params: {
       slug: blog.slug,
     },
